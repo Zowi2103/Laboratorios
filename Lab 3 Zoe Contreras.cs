b5acba9f3f20963261e41722f0a421a8c7d518b9
Zowi2103 <zoebrigittecontreras2103@gmail.com>
2026-03-04 09:11:35 -0600

    Add files via upload

---
+++ Lab 3 Zoe Contreras.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Laboratorio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            string nombreEstudiante;
+            string nombreCurso;
+ 
+            Console.Write("Ingrese su nombre: ");
+            
+            nombreEstudiante = Console.ReadLine();
+
+            Console.Write("Ingrese el nombre del curso: ");
+
+            nombreCurso = Console.ReadLine();
+        
+            Console.WriteLine("Hola,");
+            Console.WriteLine(nombreEstudiante + ", vas muy bien en el curso " + nombreCurso + ".");
+            Console.WriteLine("Gracias por su atencion");
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
+

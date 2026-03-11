83c8758320ca7565db1ae6606e452f10501f0b46
Zowi2103 <zoebrigittecontreras2103@gmail.com>
2026-03-11 08:01:20 -0600

    Create L7_ZBCS_1350426_Parte2.cs

---
+++ Laboratorio 7/L7_ZBCS_1350426_Parte2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+  static void Main()
+    {
+        int pinbueno = 1234;
+        int pin = 0;
+        int intentos = 0;
+        int maxintentos = 3;
+
+        do
+        {
+            Console.Write("Ingrese el PIN: ");
+            pin = int.Parse(Console.ReadLine());
+
+            if (pin == pinbueno)
+            {
+                Console.WriteLine("Bienvenido");
+                break; 
+            }
+            else
+            {
+                Console.WriteLine("PIN incorrecto");
+            }
+
+            intentos++;
+
+        } while (intentos < maxintentos);
+
+        if (intentos == maxintentos && pin != pinbueno)
+        {
+            Console.WriteLine("Cuenta bloqueada");
+        }
+    }
+}
